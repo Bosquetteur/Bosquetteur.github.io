085d76eda30171908bd3f32853c6ccd601aeb5b8
Bosquetteur <44269884+Bosquetteur@users.noreply.github.com>
2018-10-19 14:58:20 +0100

    Photoswip good

--- photoswipe.js
+++ photoswipe.js
@@ -107,12 +107,14 @@ var framework = {
 		/*
 			elastic: {
 				out: function ( k ) {
+
 					var s, a = 0.1, p = 0.4;
 					if ( k === 0 ) return 0;
 					if ( k === 1 ) return 1;
 					if ( !a || a < 1 ) { a = 1; s = p / 4; }
 					else s = p * Math.asin( 1 / a ) / ( 2 * Math.PI );
 					return ( a * Math.pow( 2, - 10 * k) * Math.sin( ( k - s ) * ( 2 * Math.PI ) / p ) + 1 );
+
 				},
 			},
 			back: {
@@ -1769,7 +1771,7 @@ var _gestureStartTime,
 			if(!_direction && !_moved && !_isZooming) {
 
 				if(_mainScrollPos.x !== _slideSize.x * _currPositionIndex) {
-					// if main scroll position is shifted – direction is always horizontal
+					// if main scroll position is shifted â€“ direction is always horizontal
 					_direction = 'h';
 				} else {
 					var diff = Math.abs(touchesList[0].x - _currPoint.x) - Math.abs(touchesList[0].y - _currPoint.y);
